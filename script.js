18515bbcd7f96e3a2b7cd7d37b0cc266b980dab0
lakhdar hafsi <lacdart2@hotmail.com>
2023-05-28 21:22:23 +0200

    finished app

---
+++ script.js
@@ -0,0 +1,77 @@
+
+const startElement = document.getElementById("start")
+const stopElement = document.getElementById("stop")
+const resetElement = document.getElementById("reset")
+const timerElement = document.getElementById("timer")
+const titleElement = document.getElementById("title")
+
+
+
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    timerElement.innerHTML = formattedTime
+}
+
+
+function playBuzzSound() {
+    const buzzSound = document.getElementById('buzzSound');
+    buzzSound.currentTime = 0; // Reset the audio to the beginning
+    buzzSound.play();
+}
+
+
+const stopBuzzButton = document.getElementById('stopButton');
+const buzzSound = document.getElementById('buzzSound');
+
+stopButton.addEventListener('click', () => {
+    buzzSound.pause();
+    buzzSound.currentTime = 0; // Reset the audio to the beginning
+    stopBuzzButton.style.visibility = "hidden";
+    startElement.style.visibility = "visible"
+    stopElement.style.visibility = "visible"
+    resetElement.style.visibility = "visible"
+    titleElement.innerText = "Pomodoro Timer"
+    titleElement.style.fontSize = "36px";
+});
+
+function startTimer() {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer()
+
+        if (timeLeft === 0) {
+            playBuzzSound();
+            clearInterval(interval)
+            titleElement.innerText = "Time IS UP!"
+            titleElement.style.fontSize = "50px";
+            startElement.style.visibility = "hidden"
+            stopElement.style.visibility = "hidden"
+            resetElement.style.visibility = "hidden"
+            stopBuzzButton.style.visibility = "visible"
+            timeLeft = 1500;
+            updateTimer()
+        }
+    }, 1000);
+}
+function stopTimer() {
+    clearInterval(interval)
+    buzzSound.stop();
+
+
+}
+function resetTimer() {
+    clearInterval(interval)
+    timeLeft = 1500;
+    updateTimer()
+    buzzSound.stop();
+}
+
+startElement.addEventListener("click", startTimer)
+stopElement.addEventListener("click", stopTimer)
+resetElement.addEventListener("click", resetTimer)
